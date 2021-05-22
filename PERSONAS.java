4e93f332ef355c0e314a9568019599b4f5443e78
Manuel <msm0010@alu.medac.es>
2021-05-22 21:15:28 +0200

    Proyecto Inicado

---
+++ PERSONAS.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica_githubii;
+
+/**
+ *
+ * @author maanu_358j11i
+ */
+public class PERSONAS {
+    
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String localidad;
+    
+    public PERSONAS(){
+        nombre = "";
+        apellido = "";
+        edad = 0;
+        localidad = "";
+    }
+    
+    public String getNombre(){
+        return nombre;
+    }
+    public String getApellido(){
+        return apellido;
+    }
+    public int getEdad(){
+        return edad;
+    }
+    public String getLocalidad(){
+        return localidad;
+    }
+    
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    public void setApellido(String apellido){
+        this.apellido = apellido;
+    }
+    public void setEdad(int edad){
+        this.edad = edad;
+    }
+    public void setLocalidad(String localidad){
+        this.localidad = localidad;
+    }
+}
---
+++ PRACTICA_GITHUBII.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica_githubii;
+
+/**
+ *
+ * @author maanu_358j11i
+ */
+public class PRACTICA_GITHUBII {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        PERSONAS anonimo = new PERSONAS();
+        
+        System.out.println(anonimo.getEdad());
+    }
+    
+}
